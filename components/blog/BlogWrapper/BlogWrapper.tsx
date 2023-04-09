ac716203c066ffbd5bab0c2c419fd7f6c4c53c68
toke <touko@testausserveri.fi>
2023-04-10 00:56:02 +0300

    Improve contrast on code blocks

--- components/blog/BlogWrapper/BlogWrapper.tsx
+++ components/blog/BlogWrapper/BlogWrapper.tsx
@@ -4,6 +4,5 @@ import { useEffect } from "react"
 
 export default function BlogWrapper({ children }: { children: JSX.Element[] }) {
     useEffect(() => hljs.highlightAll(), [])
-
     return <div className={styles.main}>{children}</div>
 }
